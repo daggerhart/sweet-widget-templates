dc2c1258520ad8eafd93e865727a623968a0fde6
Jonathan Daggerhart <jonathan@daggerhart.com>
2015-11-01 12:19:06 -0500

    making admin-ui more drop-in friendly

--- modules/admin-ui/admin-ui.php
+++ modules/admin-ui/admin-ui.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Class Sweet_Widgets_Admin_UI
+ * @link https://github.com/daggerhart/sweet-widgets
+ */
+if ( ! class_exists('Sweet_Widgets_Templates') ) :
 class Sweet_Widgets_Admin_UI {
 
 	public $version = '0.0.2';
@@ -19,14 +23,15 @@ class Sweet_Widgets_Admin_UI {
 	function admin_enqueue_scripts(){
 		$screen_id = get_current_screen()->id ;
 		if ( $screen_id == 'widgets' ) {
-			wp_enqueue_style( 'sweet-widgets-admin-ui', plugins_url( 'admin-ui.css', __FILE__ ), array(), $this->version );
-			wp_enqueue_script( 'sweet-widgets-admin-ui', plugins_url( 'admin-ui.js', __FILE__ ), array( 'jquery', 'admin-widgets' ), $this->version );
+			wp_enqueue_style( 'sweet-widgets-admin-ui', plugins_url( 'assets/admin-ui.css', __FILE__ ), array(), $this->version );
+			wp_enqueue_script( 'sweet-widgets-admin-ui', plugins_url( 'assets/admin-ui.js', __FILE__ ), array( 'jquery', 'admin-widgets' ), $this->version );
 		}
 		if ( $screen_id == 'customize' ){
-			wp_enqueue_style( 'sweet-widgets-admin-ui', plugins_url( 'admin-ui.css', __FILE__ ), array(), $this->version );
-			wp_enqueue_script( 'sweet-widgets-admin-ui-customizer', plugins_url( 'admin-ui-customizer.js', __FILE__ ), array( 'jquery', 'underscore', 'customize-controls' ), $this->version );
+			wp_enqueue_style( 'sweet-widgets-admin-ui', plugins_url( 'assets/admin-ui.css', __FILE__ ), array(), $this->version );
+			wp_enqueue_script( 'sweet-widgets-admin-ui-customizer', plugins_url( 'assets/admin-ui-customizer.js', __FILE__ ), array( 'jquery', 'underscore', 'customize-controls' ), $this->version );
 		}
 	}
 }
 
 Sweet_Widgets_Admin_UI::register();
+endif;
